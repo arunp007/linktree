7b7a0017b08d7e8f15b720607f7433a55deb42cf
arunp007 <arunp4624@gmail.com>
2022-09-30 10:10:40 +0530

    Added Some Changes

--- linkproject/linkapp/views.py
+++ linkproject/linkapp/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-  # Create your views here.
-
 def link(request):
-    return render(request,'link.html')  
+  return render(request, 'linktree.html') 
 
 
